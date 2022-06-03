a70a6b4bb31f88f99193471bd8f21074ad26fa80
momodoujobe <momodou.jobe@yahoo.com>
2022-06-03 00:42:20 -0400

    added a services for weather api

---
+++ modules/custom/farm_weather/src/Services/WeatherTemperature.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\farm_weather\Services;
+
+/**
+ * The Weather Temperature service class.
+ */
+class WeatherApi {
+
+  /**
+   * The HTTP client to fetch the feed data with.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
+
+  /**
+   * Constructor for WeatherTemperature.
+   *
+   * @param \GuzzleHttp\ClientInterface $http_client
+   *   A Guzzle client object.
+   */
+  public function __construct(ClientInterface $http_client) {
+    $this->httpClient = $http_client;
+  }
+
+}
